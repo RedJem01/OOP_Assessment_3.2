2ef74fca7d7a4385e2cf71aee4a863b20d716008
Jem <79197456+RedJem01@users.noreply.github.com>
2021-05-02 17:03:45 +0100

    added draw function

--- Player.cs
+++ Player.cs
@@ -50,58 +50,58 @@ namespace OOP_assessment_3
 
         //Player methods
         public abstract (int, int) Play(List<Card> hand);
-        public int cardNumCheck(string card)
+        public int cardNumCheck(Card card)
         {
             int card_num = 0;
-            if (card.Contains("Ace") == true)
+            if (card.num == "Ace")
             {
                 card_num = 14;
             }
-            else if (card.Contains("2") == true)
+            else if (card.num == "2")
             {
                 card_num = 2;
             }
-            else if (card.Contains("3") == true)
+            else if (card.num == "3")
             {
                 card_num = 3;
             }
-            else if (card.Contains("4") == true)
+            else if (card.num == "4")
             {
                 card_num = 4;
             }
-            else if (card.Contains("5") == true)
+            else if (card.num == "5")
             {
                 card_num = 5;
             }
-            else if (card.Contains("6") == true)
+            else if (card.num == "6")
             {
                 card_num = 6;
             }
-            else if (card.Contains("7") == true)
+            else if (card.num == "7")
             {
                 card_num = 7;
             }
-            else if (card.Contains("8") == true)
+            else if (card.num == "8")
             {
                 card_num = 8;
             }
-            else if (card.Contains("9") == true)
+            else if (card.num == "9")
             {
                 card_num = 9;
             }
-            else if (card.Contains("10") == true)
+            else if (card.num == "10")
             {
                 card_num = 10;
             }
-            else if (card.Contains("Jack") == true)
+            else if (card.num == "Jack")
             {
                 card_num = 11;
             }
-            else if (card.Contains("Queen") == true)
+            else if (card.num == "Queen") 
             {
                 card_num = 12;
             }
-            else if (card.Contains("King") == true)
+            else if (card.num == "King")
             {
                 card_num = 13;
             }
@@ -114,35 +114,38 @@ namespace OOP_assessment_3
     {
         public override (int, int) Play(List<Card> hand)
         {
+            int j = 1;
             Console.WriteLine("Here is your hand: ");
             foreach (Card i in hand)
             {
-                Console.WriteLine(i.num + " of " + i.suit);
+                Console.WriteLine($"{j}: {i.num} of {i.suit}");
+                j++;
             }
-            Console.WriteLine("Please choose 2 cards by writing out the exact card. (rank) of (suit)");
+            Console.WriteLine("Please choose 2 cards by writing out the number before the card");
+            //Input for card 1
+            Console.WriteLine("Card 1: ");
+            int cardValue = cardInput();
+
+            //Input for card 2
+            Console.WriteLine("Card 2: ");
+            int cardValue2 = cardInput();
+                    
+            
+            return (cardValue, cardValue2);
+        }
+
+        public int cardInput()
+        {
             bool loop_done = true;
             while (loop_done == true)   //Loop to reinput if input is wrong
             {
                 try
                 {
-                    //Input for card 1
-                    Console.WriteLine("Card 1 rank: ");
-                    string card1Rank = Console.ReadLine();
-                    Console.WriteLine("Card 1 suit: ");
-                    string card1Suit = Console.ReadLine();
-                    foreach (Card i in hand)
-                    {
-                        if ((i.num == card1Rank) && (i.suit == card1Suit))
-                        {
-                            loop_done = false;
-                            int cardValue = cardNumCheck(card1Rank);
-                        }
-                        else
-                        {
-                            //If they are not in the hand then throw exception
-                            throw new WrongInputException("That is the wrong input. Please input a card in your hand.");
-                        }
-                    }
+                    string cardChoice = Console.ReadLine();
+                    Card card = hand[int.Parse(cardChoice)];
+                    loop_done = false;
+                    int cardValue = cardNumCheck(card);
+                    return cardValue;
                 }
                 catch (WrongInputException e)
                 {
@@ -150,37 +153,7 @@ namespace OOP_assessment_3
                     Console.WriteLine(e.Message);
                 }
             }
-            loop_done = true;
-            while (loop_done == true)
-            {
-                try
-                {
-                    //Input for card 2
-                    Console.WriteLine("Card 2 rank: ");
-                    string card2Rank = Console.ReadLine();
-                    Console.WriteLine("Card 2 suit: ");
-                    string card2Suit = Console.ReadLine();
-                    foreach (Card i in hand)
-                    {
-                        if ((i.num == card2Rank) && (i.suit == card2Suit))
-                        {
-                            loop_done = false;
-                            int cardValue2 = cardNumCheck(card2Rank);
-                        }
-                        else
-                        {
-                            //If they are not in the hand then throw exception
-                            throw new WrongInputException("That is the wrong input. Please input a card in your hand.");
-                        }
-                    }
-                }
-                catch (WrongInputException e)
-                {
-                    //Ouputs exception message and then loops again
-                    Console.WriteLine(e.Message);
-                }
-            }
-            return (cardValue, cardValue2);
+            return 1;
         }
     }
 
@@ -196,15 +169,15 @@ namespace OOP_assessment_3
             int rnum1 = rnd.Next(0, 9);
             Card cp_card1 = hand[rnum1];
             hand.Remove(hand[rnum1]);
-            string cpCard1Rank = cp_card1.num; 
-            int cp_card1_num = cardNumCheck(cpCard1Rank);
+            Card cpCard1 = cp_card1; 
+            int cp_card1_num = cardNumCheck(cpCard1);
 
             //Selecting a second random card
             int rnum2 = rnd.Next(0, 9);
             Card cp_card2 = hand[rnum2];
             hand.Remove(hand[rnum2]);
-            string cpCard2Rank = cp_card2.num;
-            int cp_card2_num = cardNumCheck(cpCard2Rank);
+            Card cpCard2 = cp_card2;
+            int cp_card2_num = cardNumCheck(cpCard2);
 
             return (cp_card1_num, cp_card2_num);
         }
--- Program.cs
+++ Program.cs
@@ -22,11 +22,13 @@ namespace OOP_assessment_3
 
         public void Menu()
         {
+            Human h = new Human();
             //Menu
             Console.WriteLine("*****************************");
             Console.WriteLine("* 1. Rules                  *");
             Console.WriteLine("* 2. Play against computer  *");
             Console.WriteLine("* 3. Play against player    *");
+            Console.WriteLine("* 4. Exit program           *");
             Console.WriteLine("*****************************");
             Console.WriteLine("\n Please input your choice");
             //Checking if they want to play against a computer or another player or want to see the rules
@@ -38,18 +40,77 @@ namespace OOP_assessment_3
                     string choice = Console.ReadLine();
                     if (choice == "1")
                     {
-                        //IRules
+                        //Rules
                         Console.WriteLine("Rules: Each player gets 10 cards. When it's your go you choose two cards. The program adds up your two cards and the other player's two cards and whoever's total is higher wins the hand.");
+                        Menu();
                     }
                     else if (choice == "2")
                     {
                         string opponent = "P";
-                        PvC(opponent);
+                        for (int i = 1; i < 11; i++)
+                        {
+                            Console.WriteLine($"Round {i}: ");
+                            PvP(opponent);
+                            if (h.Score > h.Score2)
+                            {
+                                Console.WriteLine($"Player 1 won with a score of {h.Score}. Player 2's score was {h.Score2}.");
+                            }
+                            else if (h.Score < h.Score2)
+                            {
+                                Console.WriteLine($"Player 2 won with a score of {h.Score2}. Player 1's score was {h.Score}.");
+                            }
+                            else
+                            {
+                                bool loop = true;
+                                while (loop == true)
+                                {
+                                    (int cardNum, int cardNum2) = Draw(opponent);
+                                    if (cardNum > cardNum2)
+                                    {
+                                        Console.WriteLine($"Player 1 won with a score of {h.Score}. Player 2's score was {h.Score2}.");
+                                    }
+                                    else if (cardNum < cardNum2)
+                                    {
+                                        Console.WriteLine($"Player 2 won with a score of {h.Score}. Player 1's score was {h.Score2}.");
+                                    }
+                                }
+
+                            }
+                        }
+
                     }
                     else if (choice == "3")
                     {
                         string opponent = "C";
-                        PvP(opponent);
+                        PvC(opponent);
+                        if (h.Score > h.Score2)
+                        {
+                            Console.WriteLine($"You won with a score of {h.Score}. The computer's score was {h.Score2}");
+                        }
+                        else if (h.Score < h.Score2)
+                        {
+                            Console.WriteLine($"The computer won with a score of {h.Score2}. Your score was {h.Score}");
+                        }
+                        else
+                        {
+                            bool loop = true;
+                            while (loop == true)
+                            {
+                                (int cardNum, int cardNum2) = Draw(opponent);
+                                if (cardNum > cardNum2)
+                                {
+                                    Console.WriteLine($"Player 1 won with a score of {h.Score}. Player 2's score was {h.Score2}.");
+                                }
+                                else if (cardNum < cardNum2)
+                                {
+                                    Console.WriteLine($"Player 2 won with a score of {h.Score}. Player 1's score was {h.Score2}.");
+                                }
+                            }
+                        }
+                    }
+                    else if (choice == "4")
+                    {
+                        Environment.Exit(1);
                     }
                     else
                     {
@@ -85,7 +146,7 @@ namespace OOP_assessment_3
             int humanTotal = hCardValue1 + hCardValue2;
             Console.WriteLine($"The total of your two cards is {humanTotal}");
 
-            (int cCardValue1, int cCardValue2) = c.Play(c.hand2);
+            (int cCardValue1, int cCardValue2) = c.Play(h.hand2);
             int computerTotal = cCardValue1 + cCardValue2;
             Console.WriteLine($"The total of the computer's two cards is {computerTotal}");
 
@@ -95,10 +156,31 @@ namespace OOP_assessment_3
                 Console.WriteLine("You have the highest total so you win this hand");
                 h.Score += 1;
             }
-            else
+            else if (humanTotal < computerTotal)
             {
                 Console.WriteLine("The computer has the highest total so it wins this hand");
-                c.Score2 += 1;
+                h.Score2 += 1;
+            }
+
+            //If there is a draw
+            else
+            {
+                bool loop = true;
+                while (loop == true)
+                {
+                    Console.WriteLine("Both the totals were the same");
+                    (int cardNum, int cardNum2) = Draw(opponent);
+                    if (cardNum > cardNum2)
+                    {
+                        h.Score += 1;
+                        loop = false;
+                    }
+                    else if (cardNum < cardNum2)
+                    {
+                        h.Score2 += 1;
+                        loop = false;
+                    }
+                }
             }
         }
 
@@ -134,11 +216,57 @@ namespace OOP_assessment_3
                 Console.WriteLine("Player 1: You have the highest total so you win this hand");
                 h.Score += 1;
             }
-            else
+            else if (h1Total < h2Total)
             {
                 Console.WriteLine("Player 2:You have the highest total so you win this hand");
                 h.Score2 += 1;
             }
+            else
+            {
+                bool loop = true;
+                while (loop == true)
+                {
+                    Console.WriteLine("Both the totals were the same");
+                    (int cardNum, int cardNum2) = Draw(opponent);
+                    if (cardNum > cardNum2)
+                    {
+                        h.Score += 1;
+                        loop = false;
+                    }
+                    else if (cardNum < cardNum2)
+                    {
+                        h.Score2 += 1;
+                        loop = false;
+                    }
+                }
+            }
+        }
+
+        //Draw function for if the players have the same total or same score at the end
+        public (int, int) Draw(string opponent)
+        {
+            Deck d = new Deck();
+            Human h = new Human();
+
+            d.Shuffle();
+
+            Console.WriteLine("Player 1 here is your random card:");
+            Console.WriteLine(d.Cards[0]);
+
+            if (opponent == "P")
+            {
+                Console.WriteLine("Player 2 here is your random card:");
+            }
+            else
+            {
+                Console.WriteLine("Here is the computer's card:");
+            }
+            Console.WriteLine(d.Cards[1]);
+
+            int cardNum = h.cardNumCheck(d.Cards[0]);
+            int cardNum2 = h.cardNumCheck(d.Cards[1]);
+
+            return (cardNum, cardNum2);
         }
     }
 }
